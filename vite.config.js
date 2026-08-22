7e50decb207aab3e4a9ef74ced213ca4b11be072
Binh Thuan <binhthuan3101@gmail.com>
2026-08-22 21:18:10 +0700

    đây

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "assets/avatar.jpg",
-          dest: "assets"
-        }
-      ]
-    })
-  ],
+  plugins: [tailwindcss()], 
   build: {
     rollupOptions: {
       input: {
