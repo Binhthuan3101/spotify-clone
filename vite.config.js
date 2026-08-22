83b9b3bc843fd8bd58bbee03ba157e20661ce349
Binh Thuan <binhthuan3101@gmail.com>
2026-08-22 21:15:18 +0700

    xong

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path"; // Khai báo thêm thư viện path để định vị file html
+import { resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [tailwindcss()], // Giữ nguyên plugin Tailwind v4 của bạn
+  plugins: [
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "assets/avatar.jpg",
+          dest: "assets"
+        }
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
-        // Khai báo chính xác đường dẫn tất cả các trang HTML ở thư mục gốc của bạn
         main: resolve(__dirname, "index.html"),
         login: resolve(__dirname, "login.html"),
         register: resolve(__dirname, "register.html"),
